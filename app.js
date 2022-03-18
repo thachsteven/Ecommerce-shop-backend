6d1e4d006592ead3593dc11c9fd0bd5c4a53d050
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-18 23:42:13 +0700

    Reading Environment Variables

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 
+require("dotenv/config")
+
+const api = process.env.API_URL
+
 app.get("/", (req, res) => {
     res.send("hello API")
 } )
 
 app.listen(3000, () => {
+  console.log(api)
   console.log("Sever is running http://localhost:3000");
 });
