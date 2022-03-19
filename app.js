61bf3bda6bd2566ab3c2e80fb8f6d4becf6a7ed6
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-19 22:10:34 +0700

    Create First API Call & Parsing Json Data

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 
-require("dotenv/config")
 
-const api = process.env.API_URL
+//Middle ware
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
-    res.send("hello API")
-} )
+require("dotenv/config");
+
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "Hair dresser",
+    image: "some_url",
+  };
+
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body
+  console.log(newProduct)
+
+  res.send(newProduct);
+});
 
 app.listen(3000, () => {
-  console.log(api)
+  console.log(api);
   console.log("Sever is running http://localhost:3000");
 });
