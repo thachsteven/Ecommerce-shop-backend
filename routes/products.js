c2d0cbd667495a58c714ddc90dff1d3072f870fb
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-20 21:12:00 +0700

    Update a Product

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find().populate('category');
+  const productList = await Product.find().populate("category");
   // const productList = await Product.find().select('name image -_id');
 
   if (!productList) {
@@ -14,7 +14,7 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get(`/:id`, async (req, res) => {
-  const product = await Product.findById(req.params.id).populate('category');
+  const product = await Product.findById(req.params.id).populate("category");
 
   if (!product) {
     res.status(500).json({ success: false });
@@ -46,6 +46,37 @@ router.post(`/`, async (req, res) => {
 
   if (!product) {
     res.status(500).send("The product cannot be created");
+  }
+  res.send(product);
+});
+
+router.put("/:id", async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) {
+    return res.status(400).send("Invalid category");
+  }
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!product) {
+    return res.status(500).send("The product cannot be update");
   }
   res.send(product);
 });
