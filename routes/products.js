3280447cdd9fa9501ac2370103ea3f61ead0bc33
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-20 22:22:50 +0700

    Delete Product and Validate Id

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const { Product } = require("../models/product");
 const { Category } = require("../models/category");
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 router.get(`/`, async (req, res) => {
   const productList = await Product.find().populate("category");
@@ -51,6 +52,9 @@ router.post(`/`, async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid product id");
+  }
   const category = await Category.findById(req.body.category);
   if (!category) {
     return res.status(400).send("Invalid category");
@@ -79,6 +83,29 @@ router.put("/:id", async (req, res) => {
     return res.status(500).send("The product cannot be update");
   }
   res.send(product);
+});
+
+router.delete("/:id", (req, res) => {
+  Product.findByIdAndRemove(req.params.id)
+    .then((product) => {
+      if (product) {
+        return res.status(200).json({
+          success: true,
+          message: "The product is deleted",
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: "product not found!",
+        });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        success: false,
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
