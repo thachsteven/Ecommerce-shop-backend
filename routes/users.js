22cb60c908aa425fb1b74aa3488852498accbc12
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-21 16:10:46 +0700

    Get User and List of Users Excluding Password

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,22 @@ const router = express.Router();
 const bcrypt = require("bcryptjs")
 
 router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
 
     if(!userList) {
         res.status(500).json({success: false})
     } 
     res.send(userList);
+})
+
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash')
+    if(!user) {
+        res.status(500).json({
+            message: "Wrong ID"
+        })
+    }
+    res.status(200).send(user)
 })
 
 router.post('/', async (req, res) =>{
@@ -33,5 +43,7 @@ router.post('/', async (req, res) =>{
     res.send(user);
 
 })
+
+
 
 module.exports =router;
